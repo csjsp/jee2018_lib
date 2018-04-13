98eda7409438699b7955839a07363a184869c0b9
ljcyu <ljcyu@163.com>
2018-04-13 22:59:04 +0800

    无关文件删掉，只留jar文件

--- stu.sql
+++
@@ -1,6 +0,0 @@
-insert into stu(username,pass,os,java,math)
-values('zhangsan','123',89,87,96)
-insert into stu(username,pass,os,java,math)
-values('lisi','123',89,87,96)
-insert into stu(username,pass,os,java,math)
-values('wangwu','123',89,87,96)
